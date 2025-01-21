8f47d6de0a002c718ac96da843c6dd727e02f2b9
milan <milan@milan.wtf>
2025-01-21 15:43:31 +0100

    refactor(pdf): add parser struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use pdf::Parser;
+
 mod pdf;
 
 fn main() {
     pretty_env_logger::init();
 
-    let document = pdf::load_document("data/pdf/analysis_of_blood_flow_in_one.pdf")
+    let parser = Parser::load("data/pdf/analysis_of_blood_flow_in_one.pdf")
         .expect("could not load document");
 }
--- src/pdf.rs
+++ src/pdf.rs
@@ -1,26 +1,38 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use error::Error;
 use lopdf::Document;
 
 pub mod error;
 
-/// Load a PDF document from a specific path.
-///
-/// # Errors
-///
-/// This function will return an error if the document could not be loaded or if it is encrypted.
-pub fn load_document<P: AsRef<Path>>(path: P) -> Result<Document, Error> {
-    Document::load(&path)
-        .map_err(|err| Error::Load {
-            path: path.as_ref().to_path_buf(),
-            source: err,
+pub struct Parser {
+    path: PathBuf,
+    document: Document,
+}
+
+impl Parser {
+    /// Load a PDF document from a specific path.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if the document could not be loaded or is encrypted.
+    pub fn load<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
+        let path = path.as_ref().to_path_buf();
+
+        Ok(Self {
+            path: path.clone(),
+            document: Document::load(&path)
+                .map_err(|err| Error::Load {
+                    path: path.clone(),
+                    source: err,
+                })
+                .and_then(|doc| {
+                    if doc.is_encrypted() {
+                        Err(Error::Encrypted(path))
+                    } else {
+                        Ok(doc)
+                    }
+                })?,
         })
-        .and_then(|doc| {
-            if doc.is_encrypted() {
-                Err(Error::Encrypted(path.as_ref().to_path_buf()))
-            } else {
-                Ok(doc)
-            }
-        })
+    }
 }
