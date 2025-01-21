4b09512e068204041553ecb5df3ea96eebecc613
milan <milan@milan.wtf>
2025-01-21 15:32:21 +0100

    feat: add logging support

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod pdf;
 
 fn main() {
+    pretty_env_logger::init();
+
     let document = pdf::load_document("data/pdf/analysis_of_blood_flow_in_one.pdf")
         .expect("could not load document");
 }
